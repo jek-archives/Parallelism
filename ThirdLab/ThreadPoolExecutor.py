e8b17f0daf140ac553025efd0115a5cc673959ee
jek-archives <quejada.jakelloyd@gmail.com>
2026-02-13 14:00:03 +0000

    ThreadPoolExecutor Functions

--- ThirdLab/ThreadPoolExecutor.py
+++ ThirdLab/ThreadPoolExecutor.py
@@ -1,7 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 import threading
 
-# Deduction functions
+# Deduction computation functions
 def compute_sss(salary):
     print(f"SSS executed by {threading.current_thread().name}")
     return salary * 0.04
